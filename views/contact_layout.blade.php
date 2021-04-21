b0b2a43188dda3788c424272f33ef0c93748ee89
jserranos09 <64979937+jserranos09@users.noreply.github.com>
2021-04-21 08:49:39 -0700

    Add files via upload

---
+++ views/contact_layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- wherever a page has a "head" section it will replace this area-->
+        <!-- gets populated from contactusform-->
+        @yield('head')
+    </head>
+    <body style="padding-top: 60px">
+        <!-- wherever a page has a "body" section it will replace this area-->
+        @yield('body')
+    </body>
+</html>
+
+
